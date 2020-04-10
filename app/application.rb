458be68e125883ec74c3012820f7c4f82d92cbd1
Xiaoyi Liu <lxy878@gmail.com>
2020-04-10 07:28:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
     if req.path.match(/items/)
       item = req.path.split("/items/").last
-      puts item
+      # puts item
       if @@items.include?(item)
         resp.write item.price
       else
