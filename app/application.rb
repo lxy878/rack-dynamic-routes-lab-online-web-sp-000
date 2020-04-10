cab2f68e26bd4d43fc812523c01a637ee62a161f
Xiaoyi Liu <lxy878@gmail.com>
2020-04-10 07:24:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,7 @@ class Application
 
     if req.path.match(/items/)
       item = req.path.split("/items/").last
+      puts item
       if @@items.include?(item)
         resp.write item.price
       else
