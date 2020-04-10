257cef00d3b2b199583e8691d98bc385c04e0cfc
Xiaoyi Liu <lxy878@gmail.com>
2020-04-10 07:21:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
 
     if req.path.match(/items/)
       item = req.path.split("/items/").last
-
+      binding.pry
       if @@items.include?(item)
         resp.write item.price
       else
