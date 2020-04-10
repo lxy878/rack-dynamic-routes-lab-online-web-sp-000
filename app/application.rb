1a8ab3149517ffb8fa1e1b4d29ad5a4d6ba96b2f
Xiaoyi Liu <lxy878@gmail.com>
2020-04-10 07:10:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,20 +1,4 @@
 class Application
   @@items =[]
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-
-    if req.path.match(/items/)
-      item = req.path.splite("/items/").last
-      if @@items.include?(item)
-        resp.write item.price
-      else
-        resp.write "Item not found"
-        resp.status = 400
-      end
-    else
-      resp.write "Route not found"
-      resp.status = 404
-    end
-  end
+  def
 end
